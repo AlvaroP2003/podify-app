60569d4628ab25bdc8271c45b7f8ea97140a6e6f
Alvaro Placido <164355474+AlvaroP2003@users.noreply.github.com>
2025-06-03 16:24:11 +0200

    Added header component

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,10 +1,12 @@
+import React from "react"
 
-
+import { Outlet } from "react-router-dom"
 
 export default function MainLayout() {
     return (
         <>
-        MainLayout
+            <Header/>
+            <Outlet/>
         </>
     )
 }
