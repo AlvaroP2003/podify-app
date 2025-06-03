4029db7354ce266a96611a78f448f5153bb6799f
Alvaro Placido <164355474+AlvaroP2003@users.noreply.github.com>
2025-06-03 16:32:16 +0200

    Added main layout and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
+import { BrowserRouter, Routes,Route } from "react-router-dom"
 
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-
+import MainLayout from "./components/MainLayout"
+import Home from "./pages/Home"
+import Favourites from "./pages/Favourites"
+import User from "./pages/User"
+import PodcastDetail from "./pages/PodcastDetail"
 
 function App() {
-
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" />
-      </Routes>
-    </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<MainLayout/>}>
+                    <Route index element={<Home/>}></Route>
+                    <Route path="/favourites" element={<Favourites/>}></Route>
+                    <Route path="/user" element={<User/>}></Route>
+                    <Route path="/:id" element={<PodcastDetail/>}></Route>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
